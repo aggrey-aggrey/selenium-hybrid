4fdae4a30624da8b31fcaf6e5f0951e152b6bbd4
aggrey-aggrey <iluv2code2@gmail.com>
2018-01-17 20:27:24 -0600

    added log4j2

--- src/main/java/com/automation/testBase/TestBase.java
+++ src/main/java/com/automation/testBase/TestBase.java
@@ -1,5 +1,4 @@
 package com.automation.testBase;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -10,7 +9,6 @@ import java.util.Iterator;
 import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
@@ -26,10 +24,12 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 
+import seleniumhybrid.PracticeTest;
+
 //@SuppressWarnings("unused")
 @SuppressWarnings("unused")
 public class TestBase {
-	
+
 	public WebDriver driver;
 	public static WebDriverWait wait;
 	public Properties properties;
@@ -37,16 +37,16 @@ public class TestBase {
 	public FileInputStream fileinputstream;
 	static String driverPath = "/src/main/java/com/automation/drivers/";
 	static String configPath = "/src/main/java/com/automation/config/";
-	
+
 	public WebDriver getDriver() {
 		return this.driver;
 	}
-	
+
 	public void setDriver (String browserType, String appURL) {
 		switch (browserType) {
 		case "chrome":
 			driver = initChromeDriver(appURL);
-        break;
+			break;
 		case "firefox":
 			driver = initFirefoxDriver(appURL);
 			break;
@@ -55,8 +55,8 @@ public class TestBase {
 			break;
 		}
 	}
-	
-	
+
+
 	private static WebDriver initChromeDriver(String appURL) {
 		System.out.println("Launching google chrome with new profile..");
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + driverPath  + "chromedriver.exe");
@@ -65,9 +65,9 @@ public class TestBase {
 		driver.navigate().to(appURL);
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 		return driver;
-		
+
 	}
-	
+
 	private static WebDriver initFirefoxDriver(String appURL) {
 		System.out.println("Launching Firefox with new profile..");
 		System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") +  driverPath + "geckodriver.exe");
@@ -76,9 +76,9 @@ public class TestBase {
 		driver.navigate().to(appURL);
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 		return driver;
-		
+
 	}
-	
+
 	private static WebDriver initIEDriver(String appURL) {
 		System.out.println("Launching internetexplorer with new profile..");
 		System.setProperty("webdriver.ie.driver", System.getProperty("user.dir") + driverPath + "" );
@@ -87,44 +87,44 @@ public class TestBase {
 		driver.navigate().to(appURL);
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 		return driver;
-		
+
 	}
-	
+
 	@Parameters({"browserType", "appURL"})
 	@BeforeClass
 	public void initializeTestBase(String browserType, String appURL) {
-		
+
 		try	{
 			setDriver(browserType,appURL);
 		}catch (Exception e) {
 			System.out.println("Error....." + e.getMessage());
 		}
-		
-		
+
+
 	}
-	
+
 	public void loadPropertiesFile() throws IOException {
-			    properties = new Properties();
-				file = new File(System.getProperty("user.dir") + configPath + "config.properties");
-			    fileinputstream = new FileInputStream(file);
-			    properties.load(fileinputstream);
-			    
-			    file = new File(System.getProperty("user.dir") + configPath + "org.properties");
-			    fileinputstream = new FileInputStream(file);
-			    properties.load(fileinputstream);
+		properties = new Properties();
+		file = new File(System.getProperty("user.dir") + configPath + "config.properties");
+		fileinputstream = new FileInputStream(file);
+		properties.load(fileinputstream);
+
+		file = new File(System.getProperty("user.dir") + configPath + "org.properties");
+		fileinputstream = new FileInputStream(file);
+		properties.load(fileinputstream);
 	}
-	
+
 	public void getPropertiesData() {
-		
+
 	}
-	
-	
+
+
 	public void getaScreenShot(String imageName) {
-		
+
 		if (imageName.equals("")) {
 			imageName = "blank";
 		}
-		
+
 		File image = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 		String imageLocation = (System.getProperty("user.dir") + "/src/test/java/com/automation/screenshots/");
 		Calendar calendar =  Calendar.getInstance();
@@ -132,32 +132,32 @@ public class TestBase {
 		String actualImageName = imageLocation + imageName + "_" + formater.format(calendar.getTime()) + ".png";
 		File destFile = new File(actualImageName);
 		//FileUtils.copyFile(image,destFile);
-		
+
 	}
-	
+
 	public WebElement waitForElementClickable(WebElement locator,int timeout) {
 		WebElement element = null;
 
 		try {
-			
+
 			System.out.println("");
-			 wait = new WebDriverWait(driver,30);
+			wait = new WebDriverWait(driver,30);
 			element = wait.until(ExpectedConditions.elementToBeClickable(locator));
 			System.out.print("Element is clickable");
 		}catch(Exception e) {
 			System.out.println("Element is not clickable");
-			
+
 		}
 		return element;
-		
+
 	}
-	
-	
+
+
 	public WebElement waitForElementVisible(By locator, int timeout) {
 		WebElement element = null;
 		try {
 			System.out.println("Waiting for max: " + timeout + " seconds for element to be available");
-			
+
 			WebDriverWait wait = new WebDriverWait(driver, 3);
 			element = wait.until(
 					ExpectedConditions.visibilityOfElementLocated(locator));
@@ -167,21 +167,21 @@ public class TestBase {
 		}
 		return element;
 	}
-	
+
 	public void getWindowHandles() throws InterruptedException {
-		
+
 		//Get the handle
 		String parentHandle = driver.getWindowHandle();
 		System.out.println("Parent Handle: " + parentHandle);
-		
+
 		//Find Open Window button
 		WebElement openWindow = driver.findElement(By.xpath("//button[@id='openwindow']"));
 		openWindow.click();
-		
+
 		//Get all window handles
 		Set<String> handles = driver.getWindowHandles();
 		System.out.print(handles);
-		
+
 		//Switching between handles
 		for (String handle: handles){
 			System.out.println(handle);
@@ -189,29 +189,29 @@ public class TestBase {
 				driver.switchTo().window(handle);
 				//perform some action after switching window
 				break;
-		  }
-		  //switch back to parent window
+			}
+			//switch back to parent window
 			//driver.switchTo().window(parentHandle);	
 		}
-		
+
 	}
-	
+
 	public Iterator<String> getAllWindows(){
 		Set<String> windows = driver.getWindowHandles();
 		Iterator<String> itr = windows.iterator();
-			System.out.println(itr);
-			return itr;
-			
-		}
-		public static void main(String []args) throws IOException {
-			TestBase testbase = new TestBase();
-			testbase.loadPropertiesFile();
-			System.out.println(testbase.properties.getProperty("url"));
-			System.out.println(testbase.properties.getProperty("testname"));
-			
-		}
-		@AfterClass 
-		public void tearDown() {
-			driver.quit();
-		}
+		System.out.println(itr);
+		return itr;
+
+	}
+	public static void main(String []args) throws IOException {
+		TestBase testbase = new TestBase();
+		testbase.loadPropertiesFile();
+		System.out.println(testbase.properties.getProperty("url"));
+		System.out.println(testbase.properties.getProperty("testname"));
+
+	}
+	@AfterClass 
+	public void tearDown() {
+		driver.quit();
+	}
 }
